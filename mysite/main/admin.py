7ccc93f43625b1d9c62b8ceb61a42f010bc0c937
Hugo Fellipe Lage Alvaraes Da Cruz <hcruz@ml.com>
2021-02-06 11:04:43 -0300

    learned how to change order in admin page

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Curiosities
 
+class CuriositiesAdmin(admin.ModelAdmin):
+    fields = [
+        "author_name",
+        "published_date",
+        "curiosity"
+    ]
+
 # Register your models here.
-admin.site.register(Curiosities)
+admin.site.register(Curiosities,CuriositiesAdmin)
