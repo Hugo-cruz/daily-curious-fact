d787865e0a5a243a4f0983a9df8a2093ebcfadfc
Hugo Fellipe Lage Alvaraes Da Cruz <hcruz@ml.com>
2021-02-06 09:58:29 -0300

    added a simples model/table to store the curiositties

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Curiosities(models.Model):
+    
+    author_name = models.CharField(max_length=200)
+    curiosity = models.TextField()
+    published_date = models.DateTimeField("date published")
+    
+
+    #override to print statement
+    def __str__(self):
+        return self.curiosity
